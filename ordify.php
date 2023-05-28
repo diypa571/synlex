9b5ca687fb830c5d6d2524107b41f624519960a6
Diyar Parwana <diypa571@gmail.com>
2023-05-29 00:17:57 +0200

    Create ordify.php

---
+++ ordify.php
@@ -0,0 +1,22 @@
+<?php
+// Ha strängar som ord.. 
+function createWordArray($string) {
+  // Trimma space
+  $string = trim($string);
+
+  // Splitrar
+  $words = explode(" ", $string);
+
+  return $words;
+}
+
+// Example usage
+$string = "Startrek Enterprie är bäst, eller vad tycker?";
+$wordArray = createWordArray($string);
+
+// Print the array of words
+foreach ($wordArray as $word) {
+  echo $word . "<br>";
+}
+
+?>
